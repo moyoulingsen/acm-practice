4ccc31fd56c5bb4707cef9adfefc6ee42672d14a
moyoulingsen <2956820431@qq.com>
2026-03-15 18:24:13 +0800

    3.15 水一天

---
+++ atcode/ABC/449/1_RE.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;  
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;cin>>n;
+    cout << fixed<<setprecision(20)<<M_PI*n*n/4;
+
+    return 0;
+}
