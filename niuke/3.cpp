c6ec9f9d28b6a37ec2ce502290b5ef3be69dd6ed
moyoulingsen <2956820431@qq.com>
2026-03-25 20:25:28 +0800

    3.21 模板乱使用

--- niuke/3.cpp
+++ niuke/3.cpp
@@ -1,42 +1,43 @@
 #include <bits/stdc++.h>
-
-#if __has_include("tool/local.hpp")
-    #include "tool/local.hpp"
-#else
-    #define debug(...) 42
-#endif
-
+#define pb push_back
+#define pf push_front
+#define pob pop_back
+#define pof pop_front
+#define eb emplace_back
+#define fi first
+#define se second
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define endl "\n"
 using namespace std;
 
-using i64 = long long;
-using u64 = unsigned long long;
-using f80 = long double;
-using pii = pair<int, int>;
-using pll = pair<i64, i64>;
+using ll = long long;
+using ld = long double;
+using ui = unsigned;
+using ull = unsigned long long;
+using i128 = __int128;
+using PII = pair<int, int>;
+using PLL = pair<ll, ll>;
+using TIII = tuple<int, int, int>;
+using TLLL = tuple<ll, ll, ll>;
 
-constexpr int INF = 1e9 + 7;
-constexpr i64 INF64 = 4e18;
+void init() {
+}
 
 void solve() {
-    int a;
-    cin >>a;
-
-    if(a<=18){cout << a+5;}
-    else {cout << a-24+5;}
+    
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-    // #ifndef ONLINE_JUDGE
-    //     (void)!freopen("data.in", "r", stdin);
-    //     (void)!freopen("data.out", "w", stdout);
-    // #endif
+    init();
 
-    int T = 1;
-    // cin >> T;
-    while (T--) {
+    int t = 1;
+    cin >> t;
+    cout << fixed << setprecision(15);
+    for (int _ = 1; _ <= t; ++_) {
         solve();
     }
 
